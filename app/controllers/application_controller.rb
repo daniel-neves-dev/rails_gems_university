b2b910ab9825146203b8711607e73920d1f36902
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-09-18 20:33:09 -0300

    feat: identity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: %i[index show]
+  before_action :authenticate_user!, except: %i[index]
 
   include Pundit::Authorization
 
