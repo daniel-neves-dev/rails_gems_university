70153c1978ae176c4a63d4b0b0f3bf510fdc87a9
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-09-19 11:09:45 -0300

    feat: identity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: %i[index]
+  before_action :authenticate_user!, except: %i[index show]
 
   include Pundit::Authorization
 
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,7 @@
 class Course < ApplicationRecord
   has_rich_text :description
 
+  validates :title, :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 99, less_than_or_equal_to: 5000, message: "must be between 100 and 5000" }
   belongs_to :user
 end
